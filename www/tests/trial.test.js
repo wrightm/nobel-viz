1601f1777d5121b3438a26b1d871f9c41162f42f
Stephen van Beek <svanbeek2357@gmail.com>
2015-03-21 12:37:41 +0000

    Added in example of jquery being loaded

--- www/tests/trial.test.js
+++ www/tests/trial.test.js
@@ -1,8 +1,13 @@
-define([], function() {
+define(['lib/jquery'], function($) {
     describe('failing test', function() {
         it('should fail', function() {
             expect('Hello World!').toEqual('require.js up and running');
         });
     });
 
+    describe('Load jquery', function () {
+	it('should log $', function() {
+	    console.log($);
+	});
+    });
 });
