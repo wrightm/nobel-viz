09399bb227c0f08a1e3153b13962bb704e4dadbb
Stephen van Beek <svanbeek2357@gmail.com>
2015-04-01 20:05:48 +0000

    Added filtering based on url

--- www/app/modules/bubbles/bubbleOverlay.js
+++ www/app/modules/bubbles/bubbleOverlay.js
@@ -4,40 +4,6 @@ define(function (require) {
     var bubbleOverlayHelpers = require("bubbleOverlayHelpers");
     var lastState = "";
 
-    function modifyBubbleOverlay(currentState,worldChart,dimension,group,points,settings){
-      if(lastState != currentState){
-        worldChart.removeCircles();
-        worldChart
-          .minRadiusWithLabel(0)
-          .minBubbleR(settings.minBubbleR)
-          .maxBubbleR(settings.maxBubbleR)
-          .fontSize(settings.fontSize)
-          .deletePoints()
-          .addPoints(points)
-          .width(settings.width)
-          .height(settings.height)
-          .dimension(dimension)
-          .group(group)
-          .radiusValueAccessor(function(p) {
-              return p.value;
-          })
-          .colors(["#ff7373","#ff4040","#ff0000","#bf3030","#a60000"])
-          .colorDomain([13, 30])
-          .colorAccessor(function(p) {
-              return p.value;
-          })
-          .title(function(p) {
-              return settings.message +": "+ p.key + "\n" +
-                     "laureates: " + p.value;
-          })
-          .label(function(p){
-            return p.value;
-          });
-          worldChart.render().redraw();
-      }
-      lastState = currentState;
-    }
-
     function BubbleOverlay(worldChart,datasets) {
       this.worldChart = worldChart;
       this.cityDimension = datasets.cityDimension;
@@ -51,58 +17,92 @@ define(function (require) {
       this.continentPoints = datasets.continentPoints;
     };
 
+    
+    /**
+     * Modify function which takes in state and (if different from previous state) 
+     *
+     */
+    function modifyBubbleOverlay(currentState,worldChart,dimension,group,points,settings){
+	if(lastState != currentState){
+            worldChart.removeCircles();
+            worldChart
+		.minRadiusWithLabel(0)
+		.minBubbleR(settings.minBubbleR)
+		.maxBubbleR(settings.maxBubbleR)
+		.fontSize(settings.fontSize)
+		.deletePoints()
+		.addPoints(points)
+		.width(settings.width)
+		.height(settings.height)
+		.dimension(dimension)
+		.group(group)
+		.radiusValueAccessor(function(p) {
+		    return p.value;
+		})
+		.colors(["#ff7373","#ff4040","#ff0000","#bf3030","#a60000"])
+		.colorDomain([13, 30])
+		.colorAccessor(function(p) {
+		    return p.value;
+		})
+		.title(function(p) {
+		    return settings.message +": "+ p.key + "\n" +
+			"laureates: " + p.value;
+		})
+		.label(function(p){
+		    return p.value;
+		});
+            worldChart.render().redraw();
+	}
+	lastState = currentState;
+    }
+    
     BubbleOverlay.prototype = {
-
-      render : function(scale){
-          var state = bubbleOverlayHelpers.state(scale);
-          var settings = bubbleOverlayHelpers.stateSettings(scale);
-          if(state == "CountryXLarge"){
-            modifyBubbleOverlay("CountryXLarge",this.worldChart,
-              this.countryDimension,
-              this.countryGroup,
-              this.countryPoints,
-              settings);
-          } 
-          else if(state == "CountryLarge") {
-            modifyBubbleOverlay("CountryLarge",this.worldChart,
-              this.countryDimension,
-              this.countryGroup,
-              this.countryPoints,
-              settings);
-          } 
-          else if(state == "CountryMedium") {
-            modifyBubbleOverlay("CountryMedium",this.worldChart,
-              this.countryDimension,
-              this.countryGroup,
-              this.countryPoints,
-              settings);
-          } 
-          else if(state == "CountrySmall") {
-            modifyBubbleOverlay("CountrySmall",this.worldChart,
-              this.countryDimension,
-              this.countryGroup,
-              this.countryPoints,
-              settings);
-          }
-          else if(state == "CityLarge"){
-            modifyBubbleOverlay("CityLarge",this.worldChart,
-              this.cityDimension,
-              this.cityGroup,
-              this.cityPoints,
-              settings);
-          } else if(state == "CityMedium"){
-            modifyBubbleOverlay("CityMedium",this.worldChart,
-              this.cityDimension,
-              this.cityGroup,
-              this.cityPoints,
-              settings);
-          }
-          
-      }
-
+	render: function(scale) {
+            var state = bubbleOverlayHelpers.state(scale);
+            var settings = bubbleOverlayHelpers.stateSettings(scale);
+            if(state == "CountryXLarge"){
+		modifyBubbleOverlay("CountryXLarge",this.worldChart,
+				    this.countryDimension,
+				    this.countryGroup,
+				    this.countryPoints,
+				    settings);
+            } 
+            else if(state == "CountryLarge") {
+		modifyBubbleOverlay("CountryLarge",this.worldChart,
+				    this.countryDimension,
+				    this.countryGroup,
+				    this.countryPoints,
+				    settings);
+            } 
+            else if(state == "CountryMedium") {
+		modifyBubbleOverlay("CountryMedium",this.worldChart,
+				    this.countryDimension,
+				    this.countryGroup,
+				    this.countryPoints,
+				    settings);
+            } 
+            else if(state == "CountrySmall") {
+		modifyBubbleOverlay("CountrySmall",this.worldChart,
+				    this.countryDimension,
+				    this.countryGroup,
+				    this.countryPoints,
+				    settings);
+            }
+            else if(state == "CityLarge"){
+		modifyBubbleOverlay("CityLarge",this.worldChart,
+				    this.cityDimension,
+				    this.cityGroup,
+				    this.cityPoints,
+				    settings);
+            } else if(state == "CityMedium"){
+		modifyBubbleOverlay("CityMedium",this.worldChart,
+				    this.cityDimension,
+				    this.cityGroup,
+				    this.cityPoints,
+				    settings);
+            }
+	}
     };
-
+    
     return BubbleOverlay;
 });
-
-
---
+++ www/tests/trial.test.js
@@ -0,0 +1,33 @@
+define(['jquery', 'bubbleOverlay', 'url'], function($, bubbleOverlay, url) {
+    describe('Test bubbleOverlay', function () {
+	it('Test modify bubble overlay function', function() {
+	    var worldChart;
+	    var data = {'cityDimension':'cityDimension',
+			'cityGroup':'cityGroup',
+			'cityPoints':'cityPoints',
+			'countryDimension':'countryDimension',
+			'countryGroup':'countryGroup',
+			'countryPoints':'countryPoints',
+			'continentDimension':'continentDimension',
+			'continentGroup':'continentGroup',
+			'continentPoints':'continentPoints'};
+	    var bubbles = new bubbleOverlay(worldChart, data);
+	    console.log(bubbles.lastState);
+	});
+    });
+
+    describe('Test url filtering', function () {
+	it('Test the filtering of a url', function() {
+	    var queryString = 'first=1&second=scnd&third=hello%20world';
+	    var filterResult = url.filterURLParams(queryString);
+
+	    expect(filterResult['first']).toEqual('1');
+	    expect(filterResult['second']).toEqual('scnd');
+	    expect(filterResult['third']).toEqual('hello world');
+	});
+    });
+    
+});
+
+
+       
