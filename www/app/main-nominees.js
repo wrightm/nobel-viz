21b9b56b289738331303e4d7cf88d73d020c54c8
wrightm <wright1michael@gmail.com>
2015-04-21 21:06:55 +0100

    added social media intergration

--- www/app/main-nominees.js
+++ www/app/main-nominees.js
@@ -107,6 +107,42 @@ define(function (require) {
     $(this).tab('show')
   });
 
+  jquery('#facebook-share').click(function (e) {
+    var url = document.location.href;
+    var title = 'Data Visualisation';
+    window.open('http://www.facebook.com/share.php?u='+url+'&title='+title, '_blank');
+  });
+
+  jquery('#google-share').click(function (e) {
+    var url = document.location.href;
+    var title = 'Data Visualisation';
+    window.open('https://plus.google.com/share?url='+url, '_blank');
+  });
+
+  jquery('#linkedin-share').click(function (e) {
+    var url = document.location.href;
+    var title = 'Data Visualisation';
+    window.open('http://www.linkedin.com/shareArticle?mini=true&url='+url+'&title='+title, '_blank');
+  });
+
+  jquery('#pinterest-share').click(function (e) {
+    var url = document.location.href;
+    var title = 'Data Visualisation';
+    window.open('http://pinterest.com/pin/create/bookmarklet/?url='+url+'&is_video=false&description='+title, '_blank');
+  });
+
+  jquery('#reddit-share').click(function (e) {
+    var url = document.location.href;
+    var title = 'Data Visualisation';
+    window.open('http://www.reddit.com/submit?url='+url+'&title='+title, '_blank');
+  });
+
+  jquery('#twitter-share').click(function (e) {
+    var url = document.location.href;
+    var title = 'Data Visualisation';
+    window.open('http://twitter.com/intent/tweet?status='+title+'+'+url, '_blank');
+  });
+  
   jquery(function() {
      setTimeout(function(){
       dc.filterAll();
